5b4b647216ef8d6f1f7db31116624a15c124e724
dtinsley333 <neecetinsley@icloud.com>
2016-11-14 16:14:30 -0600

    initial Commit

---
+++ src/CustomerService/EmployeeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using CustomerServiceConsole;
+
+
+namespace CustomerServiceTests
+{
+    public class EmployeeTests
+    {
+
+
+        Employee emp = new Employee
+        {
+            EmployeeId = 123,
+            FirstName = "Bob",
+            LastName = "Smith",
+            DepartmentName = "Accounting"
+        };
+
+
+        [Fact]
+        public void TrueTest()
+        {
+            bool isMonday = true;
+            Assert.True(isMonday);
+        }
+
+        [Fact]
+        public void CanCreateEmployee()
+        {
+            Assert.NotNull(emp);
+        }
+
+        [Fact]
+        public void EmployeeHasADepartment()
+        {
+            Assert.NotNull(emp.DepartmentName);
+        }
+    }
+}
+
--- src/CustomerServiceConsole/Program.cs
+++ src/CustomerServiceConsole/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CustomerService
+namespace CustomerServiceConsole
 {
-    public class Class1
+    public class Program
     {
-        public Class1()
+        public static void Main(string[] args)
         {
         }
     }
