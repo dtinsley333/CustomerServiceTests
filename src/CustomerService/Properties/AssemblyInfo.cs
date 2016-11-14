0f68cc467e44c88105a8c471655e7ed2a8a1905c
dtinsley333 <neecetinsley@icloud.com>
2016-11-14 16:17:58 -0600

    Changed the name of the test assembly

--- src/CustomerService/Properties/AssemblyInfo.cs
+++ src/CustomerService/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomerService")]
+[assembly: AssemblyProduct("CustomerServiceTests")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
